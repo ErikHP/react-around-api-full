049fda924186b706035e0bff618eade417c2625f
Erik Pulatov <epulatov@gmail.com>
2021-11-11 15:47:21 -0600

    Fix status code issue 403 and 500

--- backend/errors/conflictError.js
+++ backend/errors/conflictError.js
@@ -1,9 +1,10 @@
 module.exports = (err, req, res, next) => {
-  let { statusCode = 500 } = err;
-  if (err.name === "ValidationError") statusCode = 400;
-  else if (err.name === "MongoError" && err.code === 11000) statusCode = 409;
-
-  const { message = "An error occurred on the server" } = err;
-
-  res.status(statusCode).send({ message });
+  const { statusCode = 500, message } = err;
+  res
+    .status(statusCode)
+    .send({
+      error: statusCode === 500
+        ? 'Internal Server Error'
+        : message,
+    });
 };
